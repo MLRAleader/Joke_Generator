cf15503a6284befa5429fb55c034c0c04f94a5f8
mlraleader <mouwenlegan@gmail.com>
2024-07-16 06:44:52 +0100

    index.js:Rm unnecessary comments

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Définir EJS comme moteur de template
 app.set('view engine', 'ejs');
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
+
 app.get("/", async (req, res) => {
   res.render("index");
 });
